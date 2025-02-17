b3b87d2e1d7685a09f107328fb6ca524d80c746a
FirehawkenZul <65619923+FirehawkenZul@users.noreply.github.com>
2025-02-17 16:23:58 -0600

    Complete data_check step

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,12 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    # Checks size of data set to make sure size is acceptable
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data, min_price, max_price):
+    # Checks that data price range is between min and max price
+    assert data['price'].between(min_price, max_price).all()
+     
